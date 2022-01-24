86de21c2d6536682cd5e085fb186727a71c01620
Darkdevil <90670797+DarkDevil1265@users.noreply.github.com>
2022-01-24 12:56:21 +0530

    Update dice.py

--- plugins/paulwalker_tg/dice.py
+++ plugins/paulwalker_tg/dice.py
@@ -15,7 +15,7 @@ DICE_E_MOJI = "🎲"
     f_onw_fliter
 )
 async def roll_dice(client, message):
-    """ @RollaDie """
+    """ @Movvie_Hall """
     rep_mesg_id = message.message_id
     if message.reply_to_message:
         rep_mesg_id = message.reply_to_message.message_id
